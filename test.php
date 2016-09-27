6b6d9c0d9b940606dfc1eace86c9a943d2293dd0
daniel-shen <daniel.shen@skyridge.us>
2016-09-28 01:48:59 +0800

    Add files via upload

---
+++ test.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+</head>
+<body>
+<h1>Test Page 1</h1>
+<form name="input" action="assets/py/myscript-1.py" method="get">
+<input type="submit" value="Submit">
+</form>
+
+<?php 
+echo "<h2>PHP is Fun! HELLO</h2>";
+echo ""
+?> 
+
+</body>
+</html>
